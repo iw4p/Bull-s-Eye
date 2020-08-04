4e7d19bf33fb4773a0d7e012eae0679003be0d0b
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-08-04 21:28:43 +0430

    add score and round counter

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,15 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
-    
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+
     var currentValue: Int = 50
     var targetValue = 0
+    var score = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        round += 1
         targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -28,7 +45,13 @@ class ViewController: UIViewController {
     
 
     @IBAction func showAlert(_ sender: Any) {
-        let message = "The value of the slider is \(currentValue)" + "\nThe target value is: \(targetValue)"
+        
+        let diffrence = abs(targetValue - currentValue)
+        let points = 100 - diffrence
+        
+        score += points
+        
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(title: "hello, world", message: message, preferredStyle: .alert)
         
@@ -37,6 +60,9 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
     }
     
 }
