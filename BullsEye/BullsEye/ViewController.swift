55a10d68308d1e5d854771e74dc3e6554fb2a5f0
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-08-05 18:37:20 +0430

    finish app

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -51,6 +51,11 @@ class ViewController: UIViewController {
         score = 0
         round = 0
         startNewRound()
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
     }
     
     func updateLabels() {
